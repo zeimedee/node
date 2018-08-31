8ad96e29f8b6b03f0a7bd919a225be4e750409ff
zeimedee <42755623+zeimedee@users.noreply.github.com>
2018-08-31 23:35:12 +0000

    server

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hello node js');
+var http = require("http");
+http.createServer(function(request, response){
+
+    //send the http header
+    //http status:200:ok
+    //content type: text/plain
+    response.writeHead(200,{'Conten-Type': 'text/plain'});
+
+    //send the reponse body as 'hello world'
+    response.end('Hello World\n');
+}).listen(8081);
+    //console will print the message
+console.log('server running at http://127.0.0.1:8081/');
