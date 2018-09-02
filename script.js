56b0e52b9330d023da4172f5ac2c29b34ff897cd
zeimedee <42755623+zeimedee@users.noreply.github.com>
2018-09-02 21:56:06 +0000

    new news

---
+++ script.js
@@ -0,0 +1,17 @@
+var list = ["banana", "apples", "oranges", "blueberries"];
+var list2 =["banana",["apples",["oranges"], "blueberries"]];
+
+
+var user = {
+    name: "alex",
+    age: 23,
+    hobby:"anime",
+    isMarried: false,
+    spells:[
+        "firestorm",
+        "icicleblast",
+        "earthquake",
+        "tornedo"
+    ]
+};
+
