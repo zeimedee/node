e1a74d5d54c323863dcfca172716d7d9a692275e
zeimedee <42755623+zeimedee@users.noreply.github.com>
2018-09-08 22:47:03 +0000

    dom

---
+++ dom/main.js
@@ -0,0 +1,7 @@
+var button =document.getElementById("enter");
+var input = document.getElementById("input");
+button.addEventListener("click", function(){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode("testing"));
+    
+})
