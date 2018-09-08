dd2a668bf6d0ee1bab0c5ab93d0d999b4fd6c4e0
zeimedee <42755623+zeimedee@users.noreply.github.com>
2018-09-08 23:27:33 +0000

    m

--- dom/main.js
+++ dom/main.js
@@ -1,7 +1,31 @@
 var button =document.getElementById("enter");
 var input = document.getElementById("input");
-button.addEventListener("click", function(){
+var ul = document.querySelector("ul");
+
+function inputLength(){
+    return input.value.length;
+}
+
+function createListElement(){
     var li = document.createElement("li");
-    li.appendChild(document.createTextNode("testing"));
-    
-})
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
+ 
+function addListAfterClick(){
+    if(inputLength() > 0){
+    createListElement();
+}
+}
+
+function addListAfterKeyPress(event){
+    if(inputLength() > 0 && event.keyCode === 13){
+    createListElement();
+}
+}
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeyPress);
+
+
