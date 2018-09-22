625bf1f920ee75c12f14c5eb36a282bc24bf6599
zeimedee <42755623+zeimedee@users.noreply.github.com>
2018-09-22 13:26:14 +0000

    es6 free

--- es6.js
+++ es6.js
@@ -6,8 +6,8 @@ const num = () => {
     return(x) =>{
         return(y)=>{
             return x+y;
-        }
-    }
+        };
+    };
   
 
   }
@@ -22,8 +22,34 @@ var sum = (num) => num + 1;
 
 compose(sum, sum)(5);
 
-//
+// function
  const neel = () => {
      return 1+4;
      
  }
+//instantiation
+
+class Player {
+    constructor(name, type){
+        this.name = name;
+        this.type = type;
+    } introduce(){
+        console.log(`hi i am ${this.name}, and i am a ${this.type}`);
+        
+    }
+
+}
+
+class Wizard extends Player{
+    constructor(name, type){
+        super(name, type)
+    }
+    play(){
+        console.log(`weeeeeeee i am a ${this.type}`);
+        
+    }
+}
+
+const wizard1 = new Wizard('alex', 'mage');
+const wizard2 = new Wizard('jah', 'dog');
+
