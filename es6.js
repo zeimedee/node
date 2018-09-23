0290b633fec928a8e1adec7264bbb8f1d774d7e7
zeimedee <42755623+zeimedee@users.noreply.github.com>
2018-09-23 00:07:44 +0000

    practice

--- es6.js
+++ es6.js
@@ -33,7 +33,8 @@ class Player {
     constructor(name, type){
         this.name = name;
         this.type = type;
-    } introduce(){
+    } 
+    introduce(){
         console.log(`hi i am ${this.name}, and i am a ${this.type}`);
         
     }
@@ -53,3 +54,29 @@ class Wizard extends Player{
 const wizard1 = new Wizard('alex', 'mage');
 const wizard2 = new Wizard('jah', 'dog');
 
+///test
+
+class Car {
+    constructor(make, type) {
+        this.make = make;
+        this.type = type;  
+    }
+    intro(){
+        console.log(`this is a ${this.make}, ${this.type}`);
+        
+    }
+}
+
+class Driver extends Car{
+    constructor(make, type) {
+        super(make, type);
+    }
+
+    drive(){
+        console.log(`the ${this.make} ${this.type} is a fast car`);
+        
+    }
+}
+
+const driver1 = new Driver('nissan', 'navara');
+const driver2 = new Driver('toyota', 'camry');
